f0c76b869010583fe25bfe3fafb1a1f58ce8a6b7
Kirill Postrehan <themrshayzoom882@gmail.com>
2026-07-31 05:34:16 +0300

    Add docker files

--- src/visualization/api_client.py
+++ src/visualization/api_client.py
@@ -1,7 +1,8 @@
 import requests
+import os
 
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
 
 
 def get_countries():
--- src/visualization/dashboard.py
+++ src/visualization/dashboard.py
@@ -17,4 +17,4 @@ register_callbacks(app)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8050, debug=False)
